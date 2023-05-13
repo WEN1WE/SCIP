aa8d66fea2b682bc12cba23c22ff7154ff439cc2
WEN1WE <wwen8973@gmail.com>
2023-05-13 16:36:45 +0800

    6

--- CH1/exercise6.scm
+++ CH1/exercise6.scm
@@ -10,13 +10,20 @@
 (define (average x y)
 	(/ (+ x y) 2))
 
+#|
 (define (good_enough? guess x)
 	(< (abs (- (square guess) x)) 0.001))
+
+|#
+
 
 (define (sqrt x)
 	(sqrt-iter 1.0 x))
 
+
+#|
 (define (new-if predicate then-clause else-calse)
 	(cond (predicate then-clause)
 		  (else else-calse))) 
+|#
 
