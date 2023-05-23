5acc91e5240b3fca1d070a4b6113076b9b8a28e9
WEN1WE <wwen8973@gmail.com>
2023-05-23 15:31:21 +0800

    44

---
+++ CH1/exercise44.scm
@@ -0,0 +1,27 @@
+(define (smooth f)
+	(lambda (x) (/ (+ (f (- x dx)) (f x) (f (+ x dx)))) 3.0)
+)
+
+(define (compose f g)
+	(lambda (x) (f (g x)))  
+)
+
+(define (repeated f n)
+	(if (= 1 n)
+		f
+		(compose f (repeated f (- n 1))) 
+	)
+)
+
+(define (nth-smooth f n)
+	((repeated smooth 2) f)
+)
+
+
+#|
+
+
+
+不能写成 (repeated (smooth f) n) 
+因为我们需要得到的是(smooth (smooth (smooth f))) 这种形式
+|#
