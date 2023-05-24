06a6a716242255022d27cf7dec2b2d26511cf07e
WEN1WE <wwen8973@gmail.com>
2023-05-24 11:36:59 +0800

    1

--- CH2/exercise1.scm
+++ CH2/exercise1.scm
@@ -1,1 +1,20 @@
+(define (make-rat n d) 
+	(let ((g (gcd n d)))
+		(if (< d 0)
+			(cons (- (/ n g)) (- (/ d g))) 
+			(cons (/ n g) (/ d g))
+		)	
+	)
+)
 
+#|
+1 ]=> (cdr (make-rat 10 -12))
+
+;Value: 6
+
+1 ]=> (car (make-rat 10 -12))
+
+;Value: -5
+
+
+|#
