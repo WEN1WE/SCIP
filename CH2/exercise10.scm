2c62ee92033549ec3e800a62e3b35030efd7f3eb
WEN1WE <wwen8973@gmail.com>
2023-05-26 00:11:00 +0800

    10

--- CH2/exercise10.scm
+++ CH2/exercise10.scm
@@ -14,7 +14,7 @@
 
 
 (define (div-interval x y) 
-  (if (or (>= 0 (upper-bound y)) (<= 0 (lower-bound y)))
+  (if (and (>= 0 (upper-bound y)) (<= 0 (lower-bound y)))
     (error "Divide 0") 
     (mul-interval x (make-interval (/ 1.0 (upper-bound y))
                                    (/ 1.0 (lower-bound y))))
