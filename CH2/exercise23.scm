3be0b4c75ba58e71e73fdfe1a143e3d270a6e17f
WEN1WE <wwen8973@gmail.com>
2023-05-27 07:59:27 +0800

    23

---
+++ CH2/exercise23.scm
@@ -0,0 +1,16 @@
+(define (map proc items) 
+	(if (null? items)
+		nil
+      	(cons (proc (car items))
+              (map proc (cdr items)))))
+
+(define (for-each proc items)
+	(cond ((null? items) #t)
+		  (else (proc (car items)) (for-each proc (cdr items)))
+
+	)
+)
+
+(for-each (lambda (x) (newline)
+            (display x))
+          (list 57 321 88))
