0fe2e199a198b29d30a83f8495697e32e5facebb
WEN1WE <wwen8973@gmail.com>
2023-06-29 11:19:11 +0800

    38

---
+++ CH3/exercise38.scm
@@ -0,0 +1,61 @@
+(a)
+peter 110
+paul  90
+mary  45
+
+peter 110
+mary  55
+paul  35
+
+
+paul  80
+peter 90
+mary  45
+
+paul  80
+mary  40
+peter 50
+
+
+mary  50
+peter 60
+paul  40
+
+mary  50
+paul  30
+peter 40
+
+(b)
+
+1 peter +10  get1 set1
+2 paul -20   get2 set2
+3 mary /2    get3 set3
+
+
+
+对于1 get1 可以获取100 50 80     40 30
+	 set1 可以获得 110 60 90    50 40
+
+
+对于2 get2 可以获取100 110 50    55 60
+     set2 可以获得 80  90 30    35 40
+
+对于3 get3 可以获取100 110 80      90 
+     set3 可以获得 50  55 40      45
+
+
+110 90 80 60 55 50 45 40 35 30
+
+
+
+
+
+
+
+
+
+
+
+
+
+
