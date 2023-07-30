4d55bcaf42618744224cadfd4c56dec5468dd4d6
WEN1WE <wwen8973@gmail.com>
2023-07-30 08:46:09 +0800

    9

--- CH4/exercise9.scm
+++ CH4/exercise9.scm
@@ -1,37 +1,29 @@
 
 #|
-
-(while predicate
-	body
-)
+(while <predicate> <body>) 
 
 (while (< i 100)  
      (display i)  
      (set! i (+ i 1)))
 
-
+(while predicate body)
 
 (define (while predicate body)
-	(if predicate
-		(begin body (while predicate body))
+	(define while-iter (lambda (predicate-iter body-iter) 
+		                  (if predicate-iter
+		                  	 (begin body (while-iter predicate-iter body-iter))))
 	)
+	(while-iter predicate body)
 )
-
-(define while (lambda (predicate body) 
-	                  (if predicate
-	                  	 (begin body (while predicate body))))
-)
-
-
 |#
 
-(while <predicate> <body>) 
+
 
 
 (define (while->combination exp) 
 	(define lambda-while (make-lambda (list predicate body) 
-		                              (list (make-if predicate (make-begin (list body (list 'while predicate body)))) '())))
-	(make-begin (list (list 'define 'while lambda-while) exp))
+		                              (list (make-if predicate (make-begin (list body (list 'while-iter predicate body)))) '())))
+	(make-begin (list (list 'define 'while-iter lambda-while) (list 'while-iter (while-predicate exp) (while-body exp))))
 )
 
 (define (while? exp) (tagged-list? exp 'while))
