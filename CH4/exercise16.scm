f99b19dcc8a8e367037f3e500f3b06dd4d1b8aa6
WEN1WE <wwen8973@gmail.com>
2023-08-01 18:50:49 +0800

    16

---
+++ CH4/exercise16.scm
@@ -0,0 +1,33 @@
+(lambda ⟨vars⟩ 
+	(define u ⟨e1⟩) 
+	(define v ⟨e2⟩) 
+	⟨e3⟩)
+
+(lambda ⟨vars⟩
+	(let ((u '*unassigned*)
+		 (v '*unassigned*)) 
+	(set! u ⟨e1⟩)
+	(set! v ⟨e2⟩) 
+	⟨e3⟩))
+
+
+(define (lookup-variable-value var env) 
+	(define (env-loop env)
+		(define (scan vars vals) 
+			(cond ((null? vars)
+						(env-loop (enclosing-environment env))) 
+				  ((eq? var (car vars)) 
+				  		(let ((temp (car vals)))
+				  			(if (eq? temp '*unassigned*)
+				  				(error "not")
+				  				temp
+				  			)
+				  		)
+				  )
+				  (else (scan (cdr vars) (cdr vals)))))
+		(if (eq? env the-empty-environment) 
+			(error "Unbound variable" var) 
+			(let ((frame (first-frame env)))
+      			(scan (frame-variables frame)
+            		(frame-values frame)))))
+  	(env-loop env))
