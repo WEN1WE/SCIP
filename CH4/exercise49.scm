c4a7e925e47d4103d94765e939f186b6c8eaa0fa
WEN1WE <wwen8973@gmail.com>
2023-08-17 18:00:53 +0800

    49

--- CH4/exercise49.scm
+++ CH4/exercise49.scm
@@ -1,4 +1,4 @@
-(define (parse-word2 word-list) 
+(define (parse-word word-list) 
    (require (not (null? *unparsed*))) 
    (set! *unparsed* (cdr *unparsed*)) 
    (list (car word-list) (amb (cdr word-list)))) 
