e88e8f00f18c11d8dcf0046f2a9c543c4a1044c5
WEN1WE <wwen8973@gmail.com>
2023-08-25 15:59:09 +0800

    68

---
+++ CH4/exercise68.scm
@@ -0,0 +1,12 @@
+(rule (append-to-form () ?y ?y))
+(rule (append-to-form (?u . ?v) ?y (?u . ?z))
+      (append-to-form ?v ?y ?z))
+
+
+
+
+(assert! (rule (reverse () ())))
+
+(assert! (rule (reverse (?x . ?y) ?z)
+               (and (reverse ?y ?v)
+                    (append-to-form ?v (?x) ?z))))
